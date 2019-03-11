793f8f4aae07309d0d966e1535c81a55031cc448
srzvan <sbingu.razvan@gmail.com>
2019-03-11 12:42:04 +0200

    bootstrap param

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
-cat /media/mlmos-homework1/moose.txt >> /etc/systemd/homework1-service-log.txt
-echo "Initiating BOOTSTRAP.SH"
+log_file="/var/log/system-bootstrap.log"
+
+echo "Initiating BOOTSTRAP.SH" >> "$log_file"
+
+if [[ $1 -eq 0 ]]; then
+  #yum -y update &&
+  #echo "Packages updated successfully" >> "$log_file"
+  echo "Bootstrap executed with param -> $1 <-" >> "$log_file"
+fi
+
+
+
+echo "Bootstrap finished" >> "$log_file" &&
+cat /media/mlmos-homework1/moose.txt >> "$log_file"
+
+echo "Bootstrap results are available in the $log_file file."
